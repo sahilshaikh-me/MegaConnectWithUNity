7ccf23d0bedf0fc9ab10b68eea4fbfc26df85aaf
sahil shaikh <50543009+sahilshaikh-me@users.noreply.github.com>
2023-08-16 14:40:20 +0530

    Add files via upload
    https://gpailler.github.io/MegaApiClient/articles/samples.html#download-folder-content-from-a-mega-link Sample Code Line

---
+++ Plugins/Test.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CG.Web.MegaApiClient;
+using System.Linq;
+using System;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Main()
+    {
+        MegaApiClient client = new MegaApiClient();
+        client.Login("sahilshaikh@parallax.co.in", "Sah@9876");
+
+        IEnumerable<INode> nodes = client.GetNodes();
+
+        INode root = nodes.Single(x => x.Type == NodeType.Root);
+        INode myFolder = client.CreateFolder("Upload", root);
+
+        INode myFile = client.UploadFile("D://Spark ar/Lioyd Aug.glb", myFolder);
+        Uri downloadLink = client.GetDownloadLink(myFile);
+        Console.WriteLine(downloadLink);
+
+        client.Logout();
+    }
+}
